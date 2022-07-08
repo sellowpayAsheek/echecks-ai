33ce23351cf0e637bbfb8a37aa264d6f05ad623b
asheek <asheek@sellowpay.com>
2022-07-08 12:12:13 +0530

    src folder added

---
+++ src/Echeck.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Echeck;
+
+class Echeck
+{
+    public function __construct()
+    {
+        var_dump("asdas");
+    }
+
+    public function index()
+    {
+        var_dump(['id' => "asdasdas"]);
+    }
+}
