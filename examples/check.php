dae565d0c1a171de9158fd93bf5b2181a29ae802
sellowpayAsheek <101867492+sellowpayAsheek@users.noreply.github.com>
2022-08-01 16:12:18 +0530

    Update check.php

--- examples/check.php
+++ examples/check.php
@@ -4,7 +4,7 @@ use Echeck\Echeck;
 
 require_once __DIR__.'/vendor/autoload.php';
 
-$echecks = new Echeck("EiZNSqeKYpMcIZbEn3KFLDyNGKtMa7b6orEKro013a7v9TFZ6KYiOmL6QWM7","SANDBOX");
+$echecks = new Echeck("<AUTH_TOKEN>","SANDBOX");
 
 $check_pdf = $echeck->viewCheckPdf('<checkId>');
 
